6de272973581dc46f7d707b6cd8d9b98e1a8514a
VoitehovichXenia <voitehovichkg@gmail.com>
2022-01-05 09:20:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+
+    const numArr = n.toString().split('');
+    let reversedStr = '';
+
+    for (let i = numArr.length - 1; i >= 0; i--) {
+        reversedStr += numArr[i];
+    }
+    
+    return parseInt(reversedStr);
+  }
